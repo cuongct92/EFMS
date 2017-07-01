6cb1382f3d8a0d208023aca900083d3b72e4c191
cuongct92 <cuongct251992@gmail.com>
2017-07-01 13:13:36 +0700

    root
    root

---
+++ svg2gcode_command.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+//require
+var	express		=	require('express'),
+	siofu 		= 	require("socketio-file-upload")
+	app        	= 	express(),
+	fs         	= 	require('fs'),
+	server		=	require('http').createServer(app),
+    io			=	require('socket.io').listen(server),
+	argv		=	require('optimist').argv,
+	phpjs		= 	require('phpjs'),
+	Infinity	=	1e90,
+	// exec 		=	require('child_process').exec,
+	svg2gcode	=	require('./lib/svg2gcode'),
+	serialport	=	require("serialport"),
+	SerialPort	= 	serialport.SerialPort;
+	
+
+var	gcodeQueue	= 	[],
+	timer1		=	phpjs.time(),
+	machineRunning=	false,
+	okWaitTime	=	10,				//10s
+	intervalTime	=	argv.waitTime || 1500;	//1s
+
+
+
+
+//main program
+
+////read stdin data
+var svg = ""; // svg's data
+process.stdin.on('data', function(char) {
+	svg += char;
+});
+
+////start process svg data
+process.stdin.on('end', function() {
+	console.log(svg2gcode.svg2gcode(svg, argv));
+});
+
+//// end process data
+process.stdin.resume();
+
